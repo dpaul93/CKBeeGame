1fdbaf80e6ef8a532a7d525a367ca2b8d364e6de
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-16 17:08:30 +0300

    Initial commit.

---
+++ CKBeeGame/CKBeeGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CKBeeGame
+//
+//  Created by Pavlo Deynega on 16.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CKBeeGame/CKBeeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CKBeeGame
+//
+//  Created by Pavlo Deynega on 16.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
