87a1ca931a69b47c41cd10661fca4d8a03fc9666
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-16 17:17:23 +0300

    Added initial game view view controller.

---
+++ CKBeeGame/CKBeeGame/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  CKBeeGame
+//
+//  Created by Pavlo Deynega on 16.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ CKBeeGame/CKBeeGame/GameViewController.m
@@ -0,0 +1,34 @@
+//
+//  GameViewController.m
+//  CKBeeGame
+//
+//  Created by Pavlo Deynega on 16.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+#pragma amrk - Initialization
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+#pragma mark - Actions
+
+- (IBAction)hitButtonPressed:(id)sender {
+    
+}
+
+
+
+@end
