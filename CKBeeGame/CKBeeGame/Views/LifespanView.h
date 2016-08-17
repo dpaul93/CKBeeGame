3d29ce206fe6b4bfec0ad075966560164b523f80
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-17 14:51:31 +0300

    Added lifespan view.

---
+++ CKBeeGame/CKBeeGame/Views/LifespanView.h
@@ -0,0 +1,13 @@
+//
+//  LifespanView.h
+//  CKBeeGame
+//
+//  Created by Pavlo Deynega on 17.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LifespanView : UIView
+
+@end
---
+++ CKBeeGame/CKBeeGame/Views/LifespanView.m
@@ -0,0 +1,13 @@
+//
+//  LifespanView.m
+//  CKBeeGame
+//
+//  Created by Pavlo Deynega on 17.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "LifespanView.h"
+
+@implementation LifespanView
+
+@end
